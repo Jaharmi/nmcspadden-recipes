d14ede43e6377d14bf6b3868ffba9e499f01932f
Nick McSpadden <nmcspadden@gmail.com>
2015-04-08 16:28:12 -0700

    Now uses rsync to locally copy files

--- Minecraft/VerifyMinecraft.sh
+++ Minecraft/VerifyMinecraft.sh
@@ -6,13 +6,13 @@ mcpath="/Library/Application Support/minecraft"
 homepath="$HOME/Library/Application Support/"
 endpath="$HOME/Library/Application Support/minecraft/"
 
-if [[ -e "$mcpath" ]]
-then
-	if [[ ! -e "$endpath" ]]
-	then
-		cp -R "$mcpath" "$homepath"
-		chown -R $USER:staff "$endpath"
-	fi
+if [[ -e "$mcpath" ]]; then
+	/usr/bin/rsync -rtuc "$mcpath" "$endpath"
+	#r - recursive
+	#t - preserve modification dates
+	#u - update the folder, don't copy unchanged files
+	#c - calculate checksum of each file to determine if it's changed
+	chown -R $USER:staff "$endpath"
 else
 	echo "Didn't find Minecraft in /Library/Application Support/."
 fi
